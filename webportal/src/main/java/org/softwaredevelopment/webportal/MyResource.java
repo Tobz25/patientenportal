00202ff2d3eeaddc2fecf93f7200c99d4d28641a
Tobz25 <tobias_lott@t-online.de>
2017-01-14 00:17:53 +0100

    updated authentication

--- webportal/src/main/java/org/softwaredevelopment/webportal/MyResource.java
+++ webportal/src/main/java/org/softwaredevelopment/webportal/MyResource.java
@@ -5,9 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.softwaredevelopment.webportal.helper.Secured;
+
 /**
  * Root resource (exposed at "myresource" path)
  */
+@Secured
 @Path("myresource")
 public class MyResource {
 
